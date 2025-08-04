56df43a660b95809dfb49180d7cbb0acd8f9805a
F21(Fathur) <150026704+FATHURRAHMANARRIZKY@users.noreply.github.com>
2025-08-04 22:29:39 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,7 +63,7 @@
                         </a>
                         <a href="/{{ $post->slug }}"
                             class="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                            Read more
+                            Read more..
                             <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd"
